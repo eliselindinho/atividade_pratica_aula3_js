998f7319e81c47d97ba6987feb12b9f4971f18a4
elis mendonca <elis_pm@hotmail.com>
2022-08-29 11:59:38 -0300

    Atividade prática aula 3

---
+++ js/aula3.js
@@ -0,0 +1,10 @@
+let primeiroTexto = "Manipulando strings com JS";
+console.log(primeiroTexto.toUpperCase());
+console.log(primeiroTexto.slice(0, 5));
+alert(
+  primeiroTexto + " " + "tem" + " " + primeiroTexto.length + " " + "caracteres"
+);
+console.log(primeiroTexto.length);
+
+let segundoTexto = "vamos concatenar as duas strings";
+console.log(primeiroTexto + " " + segundoTexto);
